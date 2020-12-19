3afd1f3ff2adc4fc5577ca754e6128dff561d569
WouterSpekkink <wouterspekkink@gmail.com>
2020-12-19 10:33:33 +0100

    Added keybinding for firefox.

--- config.h
+++ config.h
@@ -67,12 +67,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *tabsurfcmd[] = { "tabbed", "-r 2", "surf", "-e"};
+static const char *firefoxcmd[] = { "firefox" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_s,	   spawn, 	   {.v = tabsurfcmd } },
+	{ MODKEY|ShiftMask, 		XK_f,	   spawn,          {.v = firefoxcmd } },	
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
