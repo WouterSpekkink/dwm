39c469c48e439657b8bc7c8c4fc55122940fad07
WouterSpekkink <wahsp@tutanota.com>
2021-03-22 23:29:58 +0100

    Removed lrpad from button action because it seems to make button presses more accurate in my case.

--- dwm.c
+++ dwm.c
@@ -491,7 +491,7 @@ buttonpress(XEvent *e)
 			arg.ui = 1 << i;
 		} else if (ev->x < x + blw)
 			click = ClkLtSymbol;
-		else if (ev->x > (x = selmon->ww - TEXTW(stext) + lrpad - getsystraywidth()))
+		else if (ev->x > (x = selmon->ww - TEXTW(stext) - getsystraywidth()))
 		{
 			click = ClkStatusText;
 			char *text = rawstext;
