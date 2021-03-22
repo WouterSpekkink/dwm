653d8928dee8d677e74d5b2b3a6251cc3bfa5ec3
WouterSpekkink <wahsp@tutanota.com>
2021-03-22 21:42:18 +0100

    Commented out quit cmd.

--- dwm.c
+++ dwm.c
@@ -217,7 +217,7 @@ static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *c);
 static void propertynotify(XEvent *e);
-static void quit(const Arg *arg);
+//static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void removesystrayicon(Client *i);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
@@ -1461,11 +1461,11 @@ propertynotify(XEvent *e)
 	}
 }
 
-void
+/*void
 quit(const Arg *arg)
 {
 	running = 0;
-}
+}*/
 
 Monitor *
 recttomon(int x, int y, int w, int h)
