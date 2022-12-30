91355291bc38594a3571248d1c71e955b42872d7
WouterSpekkink <wahsp@tutanota.com>
2022-12-30 13:11:04 +0100

    merged conflicts

--- config.h
+++ config.h
@@ -1,29 +1,47 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+<<<<<<< HEAD
 static const char font[] ="-misc-roboto-medium-r-*--12-*-*-*-*-*-*-*";
+=======
+>>>>>>> d583076 (Added customizations.)
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 5;        /* gap between windows */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+<<<<<<< HEAD
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
+=======
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+>>>>>>> d583076 (Added customizations.)
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_purple[]      = "#85678f";
+<<<<<<< HEAD
 static const char col_dracpurple[]  = "#bd93f9"; static const char col_dracpink[]    = "#ff79c6";
 static const char col_darkpurple[]  = "#563C5C";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_darkpurple,  col_dracpink  },
+=======
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_purple,  col_purple  },
+>>>>>>> d583076 (Added customizations.)
 };
 
 /* tagging */
@@ -36,6 +54,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+<<<<<<< HEAD
+=======
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+>>>>>>> d583076 (Added customizations.)
 };
 
 /* layout(s) */
@@ -43,13 +65,19 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
+<<<<<<< HEAD
 #include "layouts.c"
+=======
+>>>>>>> d583076 (Added customizations.)
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+<<<<<<< HEAD
 	{ "HHH",      grid },
+=======
+>>>>>>> d583076 (Added customizations.)
 };
 
 /* key definitions */
@@ -64,6 +92,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+<<<<<<< HEAD
 static const char *termcmd[]  = { "st", NULL };
 static const char *qtbrwsrcmd[] = { "qutebrowser", NULL };
 static const char *bravecmd[] = { "brave", NULL };
@@ -81,6 +110,16 @@ static Key keys[] = {
 	{ MODKEY|Mod1Mask,              XK_b,	   spawn,          {.v = bravecmd } },
 	{ MODKEY,                       XK_e, 	   spawn,          {.v = emacscmd } },
 	{ MODKEY,                       XK_z,      spawn,          {.v = zoterocmd } },
+=======
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
+
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+>>>>>>> d583076 (Added customizations.)
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
@@ -96,10 +135,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+<<<<<<< HEAD
 	{ MODKEY,                       XK_g,	   setlayout,	   {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,	   togglefullscr,  {0} },
+=======
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+>>>>>>> d583076 (Added customizations.)
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -115,7 +159,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+<<<<<<< HEAD
 	{ MODKEY|ShiftMask,             XK_q,      spawn,          SHCMD("quitcmd") },
+=======
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+>>>>>>> d583076 (Added customizations.)
 };
 
 /* button definitions */
@@ -125,11 +173,15 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+<<<<<<< HEAD
 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
     { ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
     { ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
+=======
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+>>>>>>> d583076 (Added customizations.)
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
